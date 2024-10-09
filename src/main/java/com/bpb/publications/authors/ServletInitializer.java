51a0cbbce375d8aef243328baf60f7b753a50f89
anrobe <anteromatoszabarburu@gmail.com>
2024-10-09 10:49:05 -0500

    commit 1

---
+++ src/main/java/com/bpb/publications/authors/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bpb.publications.authors;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Authors0Application.class);
+	}
+
+}
